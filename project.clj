31c111b723270c9ce13118cd7aaef06577e4c79f
Phillip Dreizen <dreizen@kupad.net>
2018-04-19 15:01:16 -0400

    1) switch to using quil from using the simple-plotter 2) changed the style to work with sequences, which I think is much more clojure-y

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [simple-plotter "0.1.2"]]
+                 [simple-plotter "0.1.2"]
+                 [quil "2.7.1"]]
   :main ^:skip-aot magic-machine.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/magic_machine/chaos/chaos1.clj
+++ src/magic_machine/chaos/chaos1.clj
@@ -1,42 +1,53 @@
 (ns magic-machine.chaos.chaos1
     (require [simple-plotter.core :as plotter])
+    (require [quil.core :as q])
     (:gen-class))
 
 ;; inspired by CHAOS1 from "The Strange Attractions of Chaos," The Magic Machine, A.K Dewdney
 
+;; our window dimensions 
+(def width 640)
+(def height 480)
 
-(defn compute [r x]
+
+;; the equation we will use to generate x values
+;; x <- rx(1-x)
+(defn f [r x]
   (* r x (- 1 x)))
 
-(defn iter [n x width y r plot?]
-  (loop [i 0,
-         x x]
-        (if (>= i n)
-          x
-          (let [next-x (compute r x)]
-            (when plot?
-              (plotter/plot (* width next-x) y))
-            (recur (inc i) next-x)))))
+;; generate x vals for a given r value
+;; we start x at 0.3
+;; we want 300 values. 
+;; we iterate 500 times and we drop the first 200 iterations "to allow transients to drop away"
+(defn genx [r]
+  (drop 200 (take 500 (iterate #(f r %) 0.3))))
 
-(defn plot-r [r width y-coord density]
-  (->
-    0.3
-    ((fn [x] (iter 200 x width y-coord r false))) ;eliminate transients
-    ((fn [x] (iter density x width y-coord r true))))) ;plot it
+;; this function returns the points to be plotted
+;; we generate r-values from 2.90 -> 4.0. 
+;; The step size will be determined by our window height. 
+;; each row on the screen represents an r value.
+;; for each r value, we will generate x values by iterating the function f (x <- rx(1-x)) 
+(defn chaos1 []
+  (->>
+    (range 2.90 4.0 (/ (- 4.0 2.90) height)) ;; generate the rvals
+    (map genx) ;;generate the xvalues for each rvalue
+    (map-indexed (fn [y xs]  ;;turn the x values into points to plot
+                     (map (fn [x] [(* width x) y]) xs)))
+    (mapcat identity))) ;;flatten points
 
+;;;
+(defn draw [points]
+  (doseq [[x y] points]
+         (q/point x y)))
 
-(def r-start 2.90)
-(def r-end 4)
+(defn setup []
+  (q/background 255))
+;;;
 
-(defn chaos1 
-  [ & {:keys [density width height] :or {density 300, width 200, height 200}}]
-  (plotter/create-window "chaos1" width height)
-  (let [r r-start
-        r-step (/ (- r-end r-start) height)]
-    (loop [r r
-           y 0]
-          (when (<= y height)
-            (plot-r r width y density)
-            (recur (+ r r-step) (inc y))))))
+(defn -main [& args]
+  (let [points (chaos1)]
+    (q/defsketch trigonometry
+      :size [width height]
+      :setup setup
+      :draw (fn [] (draw points)))))
 
-(defn -main [& args] (apply chaos1 args))
